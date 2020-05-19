2824d0e9ed0815a00c003da6390f9c69234a804a
xpertopensource <xpertopensource@gmail.com>
2020-05-19 17:19:03 +0530

    Create test.js

---
+++ test.js
@@ -0,0 +1,16 @@
+import express from 'express';
+// import log from './../../../utils/logger';
+import permit from './../../../permissions/permission';
+import * as repositoryController from './repository.controller';
+
+import {
+    catchError
+} from './../../../errors/error';
+
+let router = express.Router({
+    mergeParams: true
+});
+
+router.get('/', permit('repositories'), catchError(repositoryController.index));
+
+module.exports = router;
